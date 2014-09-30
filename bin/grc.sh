81902849639dd3734c42c961465459e35c4b1a98
Trevor Woerner <twoerner@gmail.com>
2014-09-30 08:45:15 -0400

    add startup script for gnu radio companion
    Signed-off-by: Trevor Woerner <twoerner@gmail.com>

---
+++ bin/grc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+LD_LIBRARY_PATH=/home/trevor/local/lib:/home/trevor/local/lib64:/usr/local/lib:/usr/local/lib64
+PYTHONPATH=/home/trevor/local/lib/python2.7/site-packages:/home/trevor/local/lib/python2.7/site-packages:/home/trevor/local/lib64/python2.7/site-packages
+PATH=/home/trevor/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/home/trevor/local/packages/apache-maven-2.2.1/bin
+
+/home/trevor/local/bin/gnuradio-companion
