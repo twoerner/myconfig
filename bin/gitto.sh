1c4e8365a71a0f11e9b239680683d7f66efd67c1
Trevor Woerner <twoerner@gmail.com>
2021-10-26 14:38:52 -0400

    add git cccmd and tocmd scripts for linux
    These scripts come directly from Joe Perches:
    https://lore.kernel.org/all/1473862411.32273.25.camel@perches.com/
    
    Awesome, thanks so much!
    
    Signed-off-by: Trevor Woerner <twoerner@gmail.com>

---
+++ bin/gitto.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+opts="--nogit --nogit-fallback --norolestats --pattern-depth=1"
+
+if [[ $(basename $1) =~ ^0000- ]] ; then
+    ./scripts/get_maintainer.pl --nom $opts  $(dirname $1)/*
+else
+    maint=$(./scripts/get_maintainer.pl --nol $opts $1)
+    if [ "$maint" == "" ] ; then
+	echo "linux-kernel@vger.kernel.org"
+    else
+	echo "$maint"
+    fi
+fi
