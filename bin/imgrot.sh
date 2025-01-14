24268b7c5109d53d560c74f3a2d90f7341c676fb
Trevor Woerner <twoerner@gmail.com>
2025-01-14 17:24:26 -0500

    imgrot.sh: updates
    - fix the sub command check to check for the correct sub command
      (mogrify, not jpegtran)
    - allow the user to optionally specify a rotation angle on the cmdline
      - if a non-zero rotate angle is specified on the cmdline, then skip checking
        the file itself for a rotate angle
      - if the user does not specify an angle (or an angle of zero is specified),
        check the file itself for an angle
    - allow the user to specify more than one file to process on the cmdline
    
    Signed-off-by: Trevor Woerner <twoerner@gmail.com>

--- bin/imgrot.sh
+++ bin/imgrot.sh
@@ -1,32 +1,62 @@
 #! /bin/bash
 
-if [ $# -ne 1 ]; then
-	echo "bad usage: `basename $0` <file to rotate>"
+if [ ! -x $(which mogrify) ]; then
+	echo "can't find mogrify"
 	exit 1
 fi
-
-if [ ! -x `which jpegtran` ]; then
-	echo "can't find jpegtran"
-	exit 1
-fi
-if [ ! -x `which exiftool` ]; then
+if [ ! -x $(which exiftool) ]; then
 	echo "can't find exiftool"
 	exit 1
 fi
 
-orientation=`exiftool -Orientation -S -n $1 | cut -d' ' -f2`
-rotate=0
-if [ "$orientation"x = "6"x ]; then
-	rotate=90
-fi
-if [ "$orientation"x = "8"x ]; then
-	rotate=270
-fi
 
-if [ $rotate -eq 0 ]; then
+CMDLINE=$(getopt --options=r: --longoptions=rotate: -n "$0" -- "$@")
+if [ $? -ne 0 ]; then
+	echo "getopt(1) invocation error"
 	exit 1
 fi
+eval set -- "$CMDLINE"
+unset CMDLINE
+rotate=0
+while [ 1 ]; do
+	case "$1" in
+		-r|--rotate)
+			rotate=$2
+			shift 2
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "getopt(1) internal error"
+			exit 1
+			;;
+	esac
+done
 
-mogrify -compress lossless -rotate $rotate $1
-chmod go+r "$1"
+
+while [ $# -ge 1 ]; do
+	echo -n "processing $1"
+	if [ $rotate -eq 0 ]; then
+		orientation=`exiftool -Orientation -S -n $1 | cut -d' ' -f2`
+		if [ "$orientation"x = "6"x ]; then
+			rotate=90
+		fi
+		if [ "$orientation"x = "8"x ]; then
+			rotate=270
+		fi
+	fi
+	if [ $rotate -eq 0 ]; then
+		echo "...rotate is zero, nothing to do"
+		shift
+		continue
+	fi
+
+	mogrify -compress lossless -rotate $rotate $1
+	chmod go+r "$1"
+	echo
+	shift
+done
+
 exit 0
